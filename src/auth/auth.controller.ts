fcbe8e184a5bb1c50756e29f87559552ad14bd7f
yyaremkiv <y.yaremkiv@gmail.com>
2023-04-20 12:59:13 +0300

    add jwt token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from "./auth.service";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { CreateUserDto } from "src/users/dto/create-user.dto";
 
-@Controller('auth')
-export class AuthController {}
+@ApiTags("Authorization")
+@Controller("auth")
+export class AuthController {
+  constructor(private AuthService: AuthService) {}
+
+  @Post("/login")
+  login(@Body() userDto: CreateUserDto) {
+    return this.AuthService.login(userDto);
+  }
+
+  @Post("/registration")
+  registration(@Body() userDto: CreateUserDto) {
+    return this.AuthService.registration(userDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { UsersService } from "./../users/users.service";
+import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "src/users/dto/create-user.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async login(userDto: CreateUserDto) {}
+
+  async registration(userDto: CreateUserDto) {}
+}
